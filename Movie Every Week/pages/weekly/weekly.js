5e1557cf2bb09b81a14b1da691850eb00e848089
ghostInSh3ll <15273152851@163.com>
2019-01-26 22:34:37 +0800

    0126

---
+++ Movie Every Week/pages/weekly/weekly.js
@@ -0,0 +1,13 @@
+Page({
+  
+  data:{
+    thisWeekRecommendation:{
+      name: "my bolo tie",
+      comment: "One's Must Have",
+      imagePath: "/images/myBoloTie.png"
+
+    },
+    count:123,
+    scores:58
+  }
+})
