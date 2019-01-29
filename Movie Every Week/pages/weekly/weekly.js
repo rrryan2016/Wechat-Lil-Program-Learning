b946efdeb515ad59b2b237e02ed208debb54969b
ghostInSh3ll <15273152851@163.com>
2019-01-29 18:37:01 +0800

    0129

--- Movie Every Week/pages/weekly/weekly.js
+++ Movie Every Week/pages/weekly/weekly.js
@@ -1,13 +1,20 @@
-Page({
-  
+Page({ 
   data:{
     thisWeekRecommendation:{
       name: "my bolo tie",
       comment: "One's Must Have",
-      imagePath: "/images/myBoloTie.png"
-
+      imagePath: "/images/myBoloTie.png",
+      isHighlyRecommended:true
     },
     count:123,
-    scores:58
+    scores:58,
+    weeklyRecommendationList:[
+      {
+        name:"Liu Wenzhe"
+      },
+      {
+        name:"Gong Yuan"
+      }
+    ]
   }
 })
